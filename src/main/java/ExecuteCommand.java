ca38d39343ec681d64d9ea94b795ba7ee1c1fd58
veklyyev <yevhen.veklyn@gmail.com>
2019-10-04 00:24:08 +0300

    Update ExecuteCommand.java

--- src/main/java/ExecuteCommand.java
+++ src/main/java/ExecuteCommand.java
@@ -9,7 +9,7 @@ public class ExecuteCommand {
         try {
             String CurrentDir = System.getProperty("user.dir");
             String FileName =  CurrentDir + "\\Digital_R2_MSA_Import_MD_4261.xlsm";
-            String PSCommand = "c:\\windows\\SysWOW64\\cscript.exe " + "C:\jenkins\workspace\Cutom_Test\src\test\resources\openVBScript.vbs";
+            String PSCommand = "c:\\windows\\SysWOW64\\cscript.exe " + "C:\\jenkins\\workspace\\Cutom_Test\\src\\test\\resources\\openVBScript.vbs";
             Process Result = Runtime.getRuntime().exec(PSCommand);
             Result.waitFor();
             System.out.print(Result.exitValue());
