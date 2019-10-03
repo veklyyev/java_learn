db9d79372a6faf99d610ece64c40d355d2915f93
Ihor Petruniv <ihor_petruniv@epam.com>
2019-10-03 17:03:56 +0300

    Done Using vbs script

--- excel_modify.vbs
+++ excel_modify.vbs
@@ -1,7 +1,7 @@
 MyFile = Wscript.Arguments.Item(0)
 WScript.Echo MyFile
 Set objExcel = CreateObject("Excel.Application")
-objExcel.Visible = True
+objExcel.Visible = False
 Set objWorkbook = objExcel.Workbooks.Open(MyFile)
 Set objWorksheet = objWorkbook.Worksheets(1)
 objWorksheet.Cells(1, 1).Value = Now
--- src/main/java/ExecuteCommand.java
+++ src/main/java/ExecuteCommand.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+
 public class ExecuteCommand {
     /**
      * @param args
@@ -6,8 +7,9 @@ public class ExecuteCommand {
      */
     public static void main(String[] args) throws IOException {
         try {
-            String FileName = "c:\\temp\\Digital_R2_MSA_Import_MD_4261.xlsm";
-            String PSCommand = "c:\\windows\\SysWOW64\\cscript.exe c:\\Users\\Ihor_Petruniv\\IdeaProjects\\LearnJava\\excel_modify.vbs " + FileName;
+            String CurrentDir = System.getProperty("user.dir");
+            String FileName =  CurrentDir + "\\Digital_R2_MSA_Import_MD_4261.xlsm";
+            String PSCommand = "c:\\windows\\SysWOW64\\cscript.exe " + CurrentDir + "\\excel_modify.vbs " + FileName;
             Process Result = Runtime.getRuntime().exec(PSCommand);
             Result.waitFor();
             System.out.print(Result.exitValue());
